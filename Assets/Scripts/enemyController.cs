c1155ca862b452c01e3acc7457a81d78bc8000dc
Leonardo <20171484@aloe.ulima.edu.pe>
2022-07-09 20:24:59 -0500

    scriptMovimientov1

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -12,6 +12,8 @@ public class enemyController : MonoBehaviour
     public float velocidad = 9;
     public float vidaMax;
     public float vida;
+    public float distanciaX;
+    public float distanciaY;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,67 +26,72 @@ public class enemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, player.transform.position)>=-1f)
+        Debug.Log(Mathf.Abs(transform.position.y - player.transform.position.y));
+        if (Mathf.Abs(transform.position.x - player.transform.position.x) >= distanciaX || Mathf.Abs(transform.position.y - player.transform.position.y) >= distanciaY)
         {
 
 
-            if (auxVolteado)
-            {
-                if (GetComponent<Animator>().GetInteger("estado") != estado)
-                {
-                    GetComponent<Animator>().SetInteger("estado", estado);
-                }
-                if (transform.position.x < player.transform.position.x && derecha)
-                {
-                    derecha = false;
-                    GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
-                }
-                else if (transform.position.x > player.transform.position.x && !derecha)
-                {
-                    derecha = true;
-                    GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
-                }
-                switch (estado)
-                {
-                    case 0:
-                        break;
-                    case 1:
-                        transform.position = Vector3.MoveTowards(transform.position, player.GetComponent<PlayerController>().slash.transform.position, velocidad * Time.deltaTime);
-                        break;
-                    case 2:
-                        break;
-                }
-            }
-            else
             {
 
 
-                if (GetComponent<Animator>().GetInteger("estado") != estado)
+                if (auxVolteado)
                 {
-                    GetComponent<Animator>().SetInteger("estado", estado);
+                    if (GetComponent<Animator>().GetInteger("estado") != estado)
+                    {
+                        GetComponent<Animator>().SetInteger("estado", estado);
+                    }
+                    if (transform.position.x < player.transform.position.x && derecha)
+                    {
+                        derecha = false;
+                        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+                    }
+                    else if (transform.position.x > player.transform.position.x && !derecha)
+                    {
+                        derecha = true;
+                        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+                    }
+                    switch (estado)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            transform.position = Vector3.MoveTowards(transform.position, player.GetComponent<PlayerController>().slash.transform.position, velocidad * Time.deltaTime);
+                            break;
+                        case 2:
+                            break;
+                    }
                 }
-                if (transform.position.x > player.transform.position.x && derecha)
+                else
                 {
-                    derecha = false;
-                    GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
-                }
-                else if (transform.position.x < player.transform.position.x && !derecha)
-                {
-                    derecha = true;
-                    GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
-                }
-                switch (estado)
-                {
-                    case 0:
-                        break;
-                    case 1:
-                        transform.position = Vector3.MoveTowards(transform.position, player.GetComponent<PlayerController>().slash.transform.position, velocidad * Time.deltaTime);
-                        break;
-                    case 2:
-                        break;
+
+
+                    if (GetComponent<Animator>().GetInteger("estado") != estado)
+                    {
+                        GetComponent<Animator>().SetInteger("estado", estado);
+                    }
+                    if (transform.position.x > player.transform.position.x && derecha)
+                    {
+                        derecha = false;
+                        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+                    }
+                    else if (transform.position.x < player.transform.position.x && !derecha)
+                    {
+                        derecha = true;
+                        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+                    }
+                    switch (estado)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            transform.position = Vector3.MoveTowards(transform.position, player.GetComponent<PlayerController>().slash.transform.position, velocidad * Time.deltaTime);
+                            break;
+                        case 2:
+                            break;
+                    }
                 }
             }
-    }
+        }
         else
         {
             GetComponent<Animator>().SetInteger("estado", 0);
