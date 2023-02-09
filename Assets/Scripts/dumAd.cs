948e91b534ee7eddbd542bb7cc3c84f586d7ed85
Leonardo <20171484@aloe.ulima.edu.pe>
2023-02-08 22:47:20 -0500

    ads offline

---
+++ Assets/Scripts/dumAd.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dumAd : MonoBehaviour
+{
+    public GameObject ad;
+    public Text textoTiempo;
+    public GameObject botonCerrar;
+    public float time = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textoTiempo.text = (Mathf.FloorToInt(time % 60)).ToString() + " second(s) remaining";
+        if(time>0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            botonCerrar.gameObject.SetActive(true);
+            textoTiempo.gameObject.SetActive(false);
+        }
+    }
+    public void destruir()
+    {
+        Destroy(ad);
+    }
+}
--- Assets/llamarAds.cs
+++ Assets/llamarAds.cs
@@ -4,36 +4,90 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class llamarAds : MonoBehaviour
 {
+    public GameObject dummyBanner;
+    public GameObject dummyInters;
+    public GameObject dummyReward;
     // Start is called before the first frame update
     private void Start()
     {
         if((SceneManager.GetActiveScene().name == "jugador1" || SceneManager.GetActiveScene().name == "jugador2") && !gameManager.instance.premium)
         {
-            LogicaAds.instance.MostrarBanner();
+            StartCoroutine(checkInternetConnection1());
         }
     }
     public void MostrarBanner()
     {
         if(!gameManager.instance.premium)
         {
-            LogicaAds.instance.MostrarBanner();
+            StartCoroutine(checkInternetConnection1());
         }
     }
     public void MostrarInters()
     {
         if(!gameManager.instance.premium)
         {
-            LogicaAds.instance.MostrarInters();
+            StartCoroutine(checkInternetConnection2());
+
         }
 
 
     }
     public void MostrarReward()
     {
-        
-       LogicaAds.instance.MostrarReward();
+        StartCoroutine(checkInternetConnection3());
+
 
         
 
+    }
+    IEnumerator checkInternetConnection1()
+    {
+        WWW www = new WWW("http://google.com/");
+   
+        yield return www;
+        if (www.error != null)
+        {
+            //error, mostrar dummy
+            Instantiate(dummyBanner);
+
+
+        }
+        else
+        {
+            LogicaAds.instance.MostrarBanner();
+        }
+    }
+    IEnumerator checkInternetConnection2()
+    {
+        WWW www = new WWW("http://google.com/");
+
+        yield return www;
+        if (www.error != null)
+        {
+            //error, mostrar dummy
+            Instantiate(dummyInters);
+        }
+        else
+        {
+            LogicaAds.instance.MostrarInters();
+        }
+
+    }
+    IEnumerator checkInternetConnection3()
+    {
+        WWW www = new WWW("http://google.com/");
+
+        yield return www;
+        if (www.error != null)
+        {
+            //error, mostrar dummy
+            Instantiate(dummyReward);
+
+        }
+        else
+        {
+            //do somtehing
+            LogicaAds.instance.MostrarReward();
+        }
     }
 }
